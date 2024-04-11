7f821e96606dcba2f3bccbe5b2fe4a98a9518ccd
Rama Almer Felix <quenzvezda@gmail.com>
2024-04-11 15:30:59 +0700

    Upload files name now add suffix

--- app/service/upload_service.py
+++ app/service/upload_service.py
@@ -2,11 +2,15 @@
 
 from werkzeug.utils import secure_filename
 import os
+import random
 
 
 def save_file(file, upload_folder):
     if file:
         filename = secure_filename(file.filename)
-        file.save(os.path.join(upload_folder, filename))
-        return {'message': 'File uploaded successfully'}, 200
+        name, ext = os.path.splitext(filename)
+        random_suffix = random.randint(1000000, 9999999)  # Generate a random number between 1,000,000 and 9,999,999
+        new_filename = f"{name}_{random_suffix}{ext}"  # Add the random suffix to the filename
+        file.save(os.path.join(upload_folder, new_filename))
+        return {'message': 'File uploaded successfully', 'filename': new_filename}, 200
     return {'error': 'No file provided'}, 400
