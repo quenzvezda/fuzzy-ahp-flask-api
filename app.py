786a18fca0ea4d98914e16bde9cc58969673499d
Rama Almer Felix <quenzvezda@gmail.com>
2024-04-20 17:48:07 +0700

    Change app.py server ip and port using enviroment variable

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 # app.py
-
-from flask import Flask
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=2722)
+    host = os.getenv('SERVER_IP', '0.0.0.0')
+    port = int(os.getenv('SERVER_PORT', '2722'))
+    app.run(debug=True, host=host, port=port)
